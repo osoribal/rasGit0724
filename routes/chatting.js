fcca8031dee2059cc9eb72196ea48b2c59f359f2
osoribal <osoribal@naver.com>
2015-06-10 00:57:43 +0900

    fix error 'client is not defined'
    require mysql and client

--- routes/chatting.js
+++ routes/chatting.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+//mysql
+var mysql = require('mysql');
+
+//db
+var client = mysql.createConnection({
+	user : 'root',
+	password : '123qwe'
+});
+
+client.query('USE App');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	client.query('(select * from chatting order by date desc limit 3) order by date', function(err, rows, fields) {
