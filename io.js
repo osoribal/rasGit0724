625f137496bbb00c1546ecce27d228a0e288c998
osoribal <osoribal@naver.com>
2015-07-02 01:10:31 +0900

    add io.js file
    proto of io.js

---
+++ io.js
@@ -0,0 +1,27 @@
+var io = require('socket.io')();
+
+io.on('connection', function(socket) {
+	console.log('user connected');
+
+	//join event
+	socket.on('join', function(data) {
+		socket.join(data);
+	});
+
+	//message event
+	socket.on('message', function(data) {
+		client.query('insert into chatting (link_id, sender_id, message, picture, date) values (?, ?, ?, ?, NOW())', 
+			[data.LINK_ID, data.SENDER_ID, data.MESSAGE, data.PICTURE], 
+			function(err, rows, fields) {
+    			if(err) {
+    				console.log('chatting insert error : ' + err);
+				} else {
+				}
+			}
+		);
+		data.DATE = new Date();
+		io.sockets.in(data.LINK_ID).emit('message', data);
+	});
+});
+
+module.exports = io;
