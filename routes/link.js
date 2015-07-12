c0c008ff4f76d673bb9cbdf043c95fa09f70ccaf
blackbell1540 <shy817@naver.com>
2015-07-12 17:09:42 +0900

    0712 link/reply

--- routes/link.js
+++ routes/link.js
@@ -14,14 +14,17 @@ client.query('USE App');
 
 
 
+//find partner -> if there is partner
+//set user and partner request = user_id
 router.post('/findpartner', function(req, res, next) {
+	console.log('recv data : ',req.body);
 	var partnerMail = req.body.partner_mail;
 	var userId = req.body.user_id;
 	//find partner
 	client.query('select user_id from USER where email = ?',[partnerMail], function(err, PFresult, fields){
 		if(err)
-		{ res.json(
-			{
+		{ 
+			res.json({
 				success : '0',
 				message : 'fail',
 				result : null
@@ -83,29 +86,6 @@ router.post('/findpartner', function(req, res, next) {
 	})
 });
 
-//request link to partner
-router.post('/linkreq', function(req, res, next) {
-	var partnerMail = req.body.partner_mail;
-	client.query('update USER set request = 1 where email = ?',[partnerMail], function(err, result, fields){
-		if(err)
-		{ res.json(
-			{
-				success : '0',
-				message : 'fail',
-				result : null
-			});
-		}
-		else
-		{ res.json(
-			{
-				success : '1',
-				message : 'OK',
-				result : null
-			});
-		}
-	})
-});	
-
 //check link request for me
 router.post('/checkreq', function(req, res, next) {
 	var userId = req.body.user_id;
@@ -129,26 +109,67 @@ router.post('/checkreq', function(req, res, next) {
 	})
 });
 
-router.post('/link/reply', function(req, res, next) {
-	var userId = req.body.user_id;
-	client.query('select request from USER where user_id = ?',[userId], function(err, result, fields){
-		if(err)
-		{ res.json(
+router.post('/reply', function(req, res, next) {
+	var userId = req.body.user_id;	//reply person
+	var reply = req.body.user_reply;	//reply
+	var requestId = req.body.request_id;	//request person
+	if(reply == 'yes')
+	{// link two people.
+		//set partner person : partner_id = my_id
+		client.query('update USER set partner_id = ? where user_id = ?',[userId, requestId], function(err, result, fields){
+			if(err)
 			{
-				success : '0',
-				message : 'fail',
-				result : null
-			});	
-		}
-		else
-		{ res.json(
+				res.json(
+					{
+						success : '0',
+						message : 'set_partner_fail',
+						result : null
+					});
+			}else
 			{
-				success : '1',
-				message : 'OK',
-				result : result
-			});
-		}
-	})
+				//set reply person : partner_id = request_id
+				client.query('update USER set partner_id = ? where user_id = ?', [requestId, userId], function(err, result, fields){
+					if(err)
+					{
+						res.json(
+							{
+								success : '0',
+								message : 'set_me_fail',
+								result : null
+							});
+					}else
+					{
+						//link success
+						client.query('insert into LINK (user1, user2) values (?, ?)', [userId, requestId], function(err, result, fields){
+							if(err)
+							{
+								res.json(
+									{
+										success : '0',
+										message : 'link_fail',
+										result : null
+									});
+							}else
+							{
+								res.json(
+									{
+										success : '1',
+										message : 'link_success',
+										result : result.insertId
+									});
+							}
+						});
+
+					}
+				});
+			}
+		});
+
+	}
+	else
+	{
+
+	}
 });
 
 
